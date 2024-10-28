d9d50a444455b9e5fccf7ee8a2a3be14d61c3500
pramip21 <pramip21@vt.edu>
2024-10-28 18:32:42 -0400

    Add files via upload

---
+++ full_ops.py
@@ -0,0 +1,67 @@
+# full_ops.py
+# Usage: python3 script_name.py arg1 arg2 ...
+#  Text explaining script usage
+# Parameters:
+#  arg1: description of argument 1
+#  arg2: description of argument 2
+#  ...
+# Output:
+#  A description of the script output
+#
+# Written by Pramil Patel
+# Other contributors: None
+#
+# Optional license statement, e.g., See the LICENSE file for the license.
+
+# import Python modules
+# e.g., import math # math module
+import sys # argv
+import math
+
+# "constants"
+# e.g., R_E_KM = 6378.137
+# helper functions
+
+## function description
+# def calc_something(param1, param2):
+#   pass
+
+# initialize script arguments
+# arg1 = '' # description of argument 1
+# arg2 = '' # description of argument 2
+
+# parse script arguments
+# if len(sys.argv)==3:
+#   arg1 = sys.argv[1]
+#   arg2 = sys.argv[2]
+#   ...
+# else:
+#   print(\
+#    'Usage: '\
+#    'python3 arg1 arg2 ...'\
+#   )
+#   exit()
+
+# write script below this line
+
+w=7.292115*10**-5
+
+if len(sys.argv)==3:
+    c_in = float(sys.argv[1])
+    n_wv = float(sys.argv[2])
+else:
+    print('python3 conv_ops.py c_in n_wv')
+
+c_out = n_wv
+h_out = 1
+w_out = 1
+adds = n_wv*c_in
+muls = n_wv*c_in
+divs = 0
+
+print(int(c_out)) # output channel count
+print(int(h_out)) # output height count
+print(int(w_out)) # output width count
+print(int(adds))  # number of additions performed
+print(int(muls))  # number of multiplications performed
+print(int(divs))  # number of divisions performed
